5ac8f2d538cb22c3697ff530cfbf772344d233f8
tharun001 <tharunkumar3579@gmail.com>
2020-05-03 17:00:49 +0530

    my portfolio website

---
+++ server.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template,url_for,request,redirect
+import csv
+app = Flask(__name__)
+
+#@app.route('/')
+#def hello_world():
+#   return 'Hello, World!' 
+
+@app.route('/')
+def home_page():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def works(page_name):
+    return render_template(page_name)
+def write_to_file(data):
+	with open('database.txt',mode='a') as database:
+		email=data["email"]
+		subject= data["subject"]
+		message = data["message"]
+		total = '\n'+email+'||'+subject+'||'+message
+		file= database.write(total)
+
+def write_to_csv(data):
+	with open('database.csv',newline='',mode='a') as database2:
+		email=data["email"]
+		subject= data["subject"]
+		message = data["message"]
+		csv_writer= csv.writer(database2,delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method == 'POST':
+		try:
+			data = request.form.to_dict()
+			write_to_csv(data)
+			return redirect('/thankyou.html')
+		except:
+			return "did not save in the data base"
+	else:
+		return "something went wrong"
+   
+
+
+
+
+#@app.route('/about/<username>')
+#def about_user(username=None):
+#    return render_template('index.html',name=username)
+
+
+
+
